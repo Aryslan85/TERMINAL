c2fc922a19647dfe57098f903610553de454eaf9
Aryslan85 <aryslan_85@mail.ru>
2023-05-10 12:05:28 +0300

    terminal_hm1

---
+++ HM1/skript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+pwd ;
+mkdir folder1 ;
+cd folder1 ;
+mkdir dir_1 dir_2 dir_3 ;
+cd dir_1 ;
+touch 1.txt 2.txt 3.txt 4.json 5.json ;
+mkdir dir_4 dir_5 dir_6 ;
+ls -la ;
+mv 4.json 5.json dir_4 ;
+
